a9d7cc2a888117ab72bb30ee7cd788712743525e
Lucas dos Reis Nogueira Sanquetta <45104306+lucassanquetta@users.noreply.github.com>
2019-06-05 23:47:28 -0300

    Projeto atualizado em 05/06/19 22:40

---
+++ Agenda_Medica_atual/WebContent/view/index.jsp
@@ -0,0 +1,82 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Agenda Médica Online - Index</title>
+
+    <!-- Imagehover CSS -->
+    <link rel="stylesheet" href="../resources/css/imagehover.min.css">
+    <!-- Bootstrap -->
+    <link href="../resources/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../resources/css/estilo.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+      <nav class="navbar navbar-fixed-top navbar-inverse navbar-transparente">
+        <div class="container">
+          <!-- Header -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barra-navegacao">
+              <span class="sr-only">Alterar menu</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>            
+            </button>         
+          <a href="index.jsp" class="navbar-brand" class="sombra"><b>Agenda</b></span> Medica<span style="font-size: 10px"> Online</span></a>
+          </div>
+          
+          <div class="collapse navbar-collapse" id="barra-navegacao">
+
+            <ul class="nav navbar-nav navbar-right">
+              <li> <a href="index.jsp">Home</a> </li>
+              <li> <a href="#">Contato</a> </li>
+              <li> <a href="#">Ajuda</a> </li>
+              <li> <a href="entrarRegistrar.jsp">Entrar/Registrar</a> </li>
+          </div>
+        </div>
+      </nav>
+
+    <div class="container">
+
+       <!-- Tela -->
+    <div class="capa">
+      
+      <div class="texto-capa">
+      
+      <div class="col-sm-8 card-centro">
+       <h3 class="text">Tela inicial</h3>
+      </div>
+
+      </div>
+
+    </div>
+
+      <br><br>
+
+        <footer>
+        <p>&copy;2019 - Plus AMO Company</p>
+        </footer>
+
+   </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="resources/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ Agenda_Medica_atual/src/controller/CredenciadoController.java
@@ -0,0 +1,162 @@
+package controller;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.DAOException;
+import model.Credenciado;
+import DAO.CredenciadoDAO;
+import DAO.CredenciadoDAOImpl;
+
+@WebServlet("/credenciado")
+public class CredenciadoController extends HttpServlet {
+	// private List<Jogo> lista;
+	private static final long serialVersionUID = 5931706310984055050L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) 
+			throws IOException { 
+		CredenciadoDAO credenciadoDao = new CredenciadoDAOImpl();
+		String id = req.getParameter("id");
+		String nome = req.getParameter("nome");
+		String cpf = req.getParameter("cpf");
+		String cep = req.getParameter("cep");
+		String datanasc = req.getParameter("datanasc");
+		String end = req.getParameter("end");
+		String bairro = req.getParameter("bairro");
+		String cidade = req.getParameter("cidade");
+		String estado = req.getParameter("estado");
+		String tel = req.getParameter("tel");
+		String cel = req.getParameter("cel");
+		String escolha = req.getParameter("escolha");
+		HttpSession session = req.getSession();
+		// lista = (List<Jogo>)getServletContext().getAttribute("LISTA");
+		if (id != null && !id.isEmpty()) {
+			
+			/*if ("Cadastrar".equals(cmd)) {*/	
+				int numId = Integer.parseInt(id);
+				Credenciado c = null;
+				try {	
+					c = credenciadoDao.pesquisarPorId(numId);
+				} catch (DAOException e) {
+					e.printStackTrace();
+				}
+				session.setAttribute("CREDENCIADO", c);
+			//}
+			/*else if ("remover".equals(cmd)) {
+				try {
+					jogoDao.remover(numId);
+					session.setAttribute("ENCONTRADOS", 
+							jogoDao.pesquisarTodos());
+				} catch (DAOException e) {
+					e.printStackTrace();
+				}
+			}*/
+		}
+		res.sendRedirect("./verAgenda.jsp");
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws IOException {
+		CredenciadoDAO credenciadoDao = new CredenciadoDAOImpl();
+		HttpSession session = req.getSession();
+		// lista = (List<Jogo>)getServletContext().getAttribute("LISTA");
+		// if (lista == null) { 
+		//	lista = new ArrayList<>();
+		//	getServletContext().setAttribute("LISTA", lista);
+		//}
+		try {
+			String id = req.getParameter("id");
+			if (id == null || id.isEmpty()) { 
+				id = "0";
+			}
+			String cmd = req.getParameter("cmd");
+			String nome = req.getParameter("nome");
+			int cpf = Integer.parseInt(req.getParameter("cpf"));
+			int cep = Integer.parseInt(req.getParameter("cep"));
+			String datanasc = req.getParameter("datanasc");
+			String end = req.getParameter("end");
+			String bairro = req.getParameter("bairro");
+			String cidade = req.getParameter("cidade");
+			String estado = req.getParameter("estado");
+			String tel = req.getParameter("tel");
+			String cel = req.getParameter("cel");
+			String escolha = req.getParameter("escolha");
+			int intId = Integer.parseInt(id);
+
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");	
+
+			Credenciado c = new Credenciado();
+			c.setId(intId);
+			c.setNome( nome );
+			c.setCpf(cpf);
+			c.setCep(cep);
+			c.setDatanasc(sdf.parse(datanasc));
+			c.setEnd(end);
+			c.setBairro(bairro);
+			c.setCidade(cidade);
+			c.setEstado(estado);
+			c.setTel(tel);
+			c.setCel(cel);
+			c.setEscolha(escolha);
+
+			String texto = "";
+			List<Credenciado> encontrados = new ArrayList<>();
+			if ("Cadastrar".equals(cmd)) { 
+				try {
+					credenciadoDao.adicionar(c);
+					texto = "Credenciado cadastrado com sucesso";
+					session.setAttribute("CREDENCIADO", null);
+					encontrados.addAll(credenciadoDao.pesquisarTodos());
+				} catch (DAOException e) {
+					e.printStackTrace();
+				}
+			} /*else if ("Salvar".equals(cmd)) {
+				//				for (Jogo jogo : lista) { 
+				//					if (jogo.getId() == j.getId()) {
+				//						lista.remove(jogo);
+				//						lista.add(j);
+				//						break;
+				//					}
+				//				}
+				try {
+					jogoDao.atualizar(j.getId(), j);
+					texto = "Jogo atualizado com sucesso";
+					encontrados.addAll(jogoDao.pesquisarTodos());
+					session.setAttribute("JOGO", null);
+				} catch (DAOException e) {
+					e.printStackTrace();
+				}
+			} else if ("Pesquisar".equals(cmd)) {
+				encontrados.clear();
+				//				for (Jogo jogo : lista) { 
+				//					if (jogo.getNome().contains(j.getNome())) {
+				//						encontrados.add(jogo);
+				//					}
+				//				}
+				try {
+					encontrados.addAll(
+							jogoDao.pesquisarPorNome(j.getNome()));
+				} catch (DAOException e) {
+					e.printStackTrace();
+				}
+				session.setAttribute("JOGO", null);
+			}*/
+			session.setAttribute("MENSAGEM", texto);
+			session.setAttribute("ENCONTRADOS", encontrados);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		res.sendRedirect("./verAgenda.jsp");
+	}
+}
